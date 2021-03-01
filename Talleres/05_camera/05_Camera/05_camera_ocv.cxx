0c3f598ba8f86271f6534ae69a11ce8c6298e593
Juan Felipe Marin <juancho9964@hotmail.com>
2021-03-01 11:11:27 -0500

    Se agrega taller 05. con OCV perspectiva funcional

---
+++ Talleres/05_camera/05_Camera/05_camera_ocv.cxx
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+
+int main(int argc, char** argv )
+{
+  // Get command line arguments
+  if ( argc < 2 )
+  {
+  	std::cerr << "Usage: " << argv[ 0 ] << " image_file" << std::endl;
+    return( -1 );
+
+  } // fi
+
+  // Review given command line arguments
+  std::cout << "-------------------------" << std::endl;
+  for( int a = 0; a < argc; a++ )
+    std::cout << argv[ a ] << std::endl;
+   std::cout << "-------------------------" << std::endl;
+   // Read an image
+   Mat image;
+   image = imread( argv[1], 1 );
+
+   if ( !image.data )
+   {
+      std::cerr << "Error: No image data" << std::endl;
+      return( -1);
+   
+   }// fi
+  Size size(2230,1248);
+
+ 
+
+  cv::Point2f src_p[4];
+    cv::Point2f dst_p[4];
+
+
+    src_p[0] = cv::Point2f(0, 0);
+    src_p[1] = cv::Point2f(   2230, 0);
+    src_p[3] = cv::Point2f(  2230,    1248);
+    src_p[2] = cv::Point2f(0,   1248);
+
+    dst_p[0] = cv::Point2f(  216, 639);
+    dst_p[1] = cv::Point2f(  2385,   780);
+    dst_p[2] = cv::Point2f(  162,  1909);
+    dst_p[3] = cv::Point2f(2397, 1876);
+
+
+  Mat salida;
+   salida= getPerspectiveTransform(dst_p,src_p);
+   Mat dst;
+   warpPerspective(image,dst, salida,size,INTER_LINEAR);
+
+  std::stringstream ss( argv[ 1 ] );
+  std::string basename;
+  getline( ss, basename, '.' );
+  imwrite( basename + "_camera.png", dst );
+   
+   return(0);
+}
+
+
---
+++ Talleres/05_camera/05_Camera/build/CMakeFiles/pr5_camera_itk.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/juanfelipemarinflorez/Desktop/Pontificia Universidad Javeriana/Semestres/Noveno/visión/Talleres/05_camara/05_Camera/05_camera_itk.cxx" "/Users/juanfelipemarinflorez/Desktop/Pontificia Universidad Javeriana/Semestres/Noveno/visión/Talleres/05_camara/05_Camera/build/CMakeFiles/pr5_camera_itk.dir/05_camera_itk.cxx.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ITK_IO_FACTORY_REGISTER_MANAGER"
+  "NDEBUG"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "ITKFactoryRegistration"
+  "/usr/local/include/ITK-5.1"
+  "/usr/local/include/ITK-5.1/Utilities"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")

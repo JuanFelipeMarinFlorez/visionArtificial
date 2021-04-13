4dcf90499da44a602c877508370b1b7972f3de02
Juan Felipe Marin <juancho9964@hotmail.com>
2021-04-12 23:23:23 -0500

    actualización taller 7 Umbralización hasta cumsum

--- Talleres/07_unbrarizacion/Umbrarizacion.cxx
+++ Talleres/07_unbrarizacion/Umbrarizacion.cxx
@@ -5,39 +5,62 @@
 
 using namespace cv;
 
-Otso_fun (Mat img){
+Mat cumsum(Mat nums) {
+  Mat result(Size(nums.cols, nums.rows), CV_8UC1);
+  float sum = 0;
+   MatIterator_<Vec3b> it, nIt, end, endr;
+    it = nums.begin<Vec3b>();
+    nIt = result.begin<Vec3b>();
+    end = nums.end<Vec3b>();
+    endr = result.end<Vec3b>();
+    for (; it != end && nIt != endr; it++,nIt++)
+    {
+            sum += (*it)[0];
+            (*nIt)[0] = sum;
+        
+    }
+  return result;
+}
+
+
+Mat Otso_fun (Mat img){
 
   //Se difumina una img con un filtro gaussiano
-  Mat blur = cv.GaussianBlur(img,(5,5),0)
+  Mat blur;
+  GaussianBlur(img,blur,Size(5,5),0,0);
 
   //Para encontrar el umbral normalizado y su función de distribución
   Mat hist, hist_norm;
-  hist = cv.calcHist([blur],[0],None,[256],[0,256])
-  hist_norm = hist.ravel()/hist.sum()
+  int histSize = 256;
+  float range[] = { 0, 256 }; 
+  int channels[]={0};
+  const float* histRange = { range };
+  calcHist(&blur,1,channels,Mat(),hist,1,&histSize,&histRange, true,false);
+  hist_norm = hist.reshape(1,1)/sum(hist);
 
 //A partir de aqui toca pasar de python a c++ porque está el código en python
-  Q = hist_norm.cumsum()
-  bins = np.arange(256)
+  Mat Q= cumsum(hist_norm);
+//std::cout<<hist_norm<<std::endl;
+  //std::cout<<"hola"<<std::endl;
+  //std::cout<<Q<<std::endl;
+  /*bins = np.arange(256)
   fn_min = np.inf
   thresh = -1
 
-  for(int i; 0<i<=256; i++) 
-  {
+  for(int i; 0<i<=256; i++){
     p1,p2 = np.hsplit(hist_norm,[i]) # probabilities
       q1,q2 = Q[i],Q[255]-Q[i] # cum sum of classes
       if q1 < 1.e-6 or q2 < 1.e-6:
           continue
       b1,b2 = np.hsplit(bins,[i]) # weights
-      # finding means and variances
       m1,m2 = np.sum(p1*b1)/q1, np.sum(p2*b2)/q2
       v1,v2 = np.sum(((b1-m1)**2)*p1)/q1,np.sum(((b2-m2)**2)*p2)/q2
-      # calculates the minimization function
       fn = v1*q1 + v2*q2
       if fn < fn_min:
           fn_min = fn
           thresh = i
-  }
-      
+  }*/
+  return blur;    
 }
 
 int main ( int argc, char** argv )
@@ -60,7 +83,7 @@ int main ( int argc, char** argv )
   const char* imageName = argc >=2 ? argv[1] : "lena.jpg";
 
   // Loads an image
-  src = imread( imageName , IMREAD_COLOR ); // Load an image
+  src = imread( imageName);
   if( src.empty() ) {
     printf(" Error opening image\n");
     printf(" Program Arguments: [image_name -- default lena.jpg] \n");
@@ -68,7 +91,7 @@ int main ( int argc, char** argv )
   }
 
   //Crea la matriz de destino
-  Mat dest;
+  Mat dest=Otso_fun(src);
     Size size(src.cols,src.rows);
 
 

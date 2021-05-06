1a8f2501af72e66811d8a43cc9da945ab567b5f5
Marlon Linares <marlon.linares16@outlook.com>
2021-05-06 17:50:48 -0500

    Taller Segmentación Avanzando :3

--- Talleres/TallerSegmentacionSemillas/11_seedSegmentation.cxx
+++ Talleres/TallerSegmentacionSemillas/11_seedSegmentation.cxx
@@ -4,8 +4,11 @@
 #include <iostream>
 #include <string.h>
 #include <ctime>
+#include <queue>
 
 using namespace cv;
+
+Mat segmentacion(Mat image, int seedX, int seedY);
 
 int main(int argc, char const *argv[])
 {
@@ -16,8 +19,8 @@ int main(int argc, char const *argv[])
     //3:
 
     //Verificación de Comandos
-    // forma ideal de ejecutar : ./11_seedSegmentation <image_file> <r (random seed) / s (set seed)>
-    //                                 <a (average) / v (variance)> <N° of seeds>
+    // forma ideal de ejecutar : ./11_seedSegmentation  <image_file> <N° of seeds>
+    //                                <r (random seed) / s (set seed)> <a (average) / v (variance)>
     //                            [<x coordinate1> <y coordinate1> <x coordinate2> <y coordinate2> (ETC)]
 
     if (argc < 5)
@@ -37,6 +40,7 @@ int main(int argc, char const *argv[])
     }
 
     //Leyendo las Semillas
+
     int seedX[atoi(argv[1])];
     int seedY[atoi(argv[1])];
     int numberSeeds = atoi(argv[2]);
@@ -48,11 +52,11 @@ int main(int argc, char const *argv[])
             seedX[i] = std::rand() % (image.cols) + 0;
             seedY[i] = std::rand() % (image.rows) + 0;
         }
-        for (int i = 0; i < numberSeeds; i++)
-        {
-            std::cout << "seedX: " << seedX[i] << std::endl;
-            std::cout << "seedy: " << seedY[i] << std::endl;
-        }
+        // for (int i = 0; i < numberSeeds; i++)
+        // {
+        //     std::cout << "seedX: " << seedX[i] << std::endl;
+        //     std::cout << "seedy: " << seedY[i] << std::endl;
+        // }
     }
     else if (strcmp(argv[3], "s") == 0)
     {
@@ -64,11 +68,15 @@ int main(int argc, char const *argv[])
                 seedX[i] = atoi(argv[5 + j]);
                 seedY[i] = atoi(argv[6 + j]);
             }
-            for (int i = 0; i < numberSeeds; i++)
-            {
-                std::cout << "seedX: " << seedX[i] << std::endl;
-                std::cout << "seedy: " << seedY[i] << std::endl;
-            }
+
+            // BORRAR_COUTS
+            // --------------------------------------------------
+            // for (int i = 0; i < numberSeeds; i++)
+            // {
+            //     std::cout << "seedX: " << seedX[i] << std::endl;
+            //     std::cout << "seedy: " << seedY[i] << std::endl;
+            // }
+            // --------------------------------------------------
         }
         else
         {
@@ -76,4 +84,44 @@ int main(int argc, char const *argv[])
             return (-1);
         }
     }
+
+    //----------------------------------------------------------------------
+    // Llamado a funcion de segmentar:
+    Mat prueba = segmentacion(image, 400, 245);
+}
+
+Mat segmentacion(Mat image, int seedX, int seedY)
+{
+    Mat grayImage;
+    cvtColor(image, grayImage, COLOR_BGR2GRAY);
+    Mat image_segmentated = Mat::zeros(image.rows, image.cols, CV_8UC1);
+    std::queue<Point> colaPixeles;
+    //Vec3b Intesidades = image.at<Vec3f>(seedY, seedX);
+    colaPixeles.push(Point(seedX, seedY));
+    image_segmentated.at<Vec3b>(seedY, seedX)[0] = 255;
+    do
+    {
+        Point punto_actual = colaPixeles.pop();
+        int prom = -1;
+        int lista_intensidades[8];
+        if (punto_actual.x > 0 && punto_actual.y > 0)
+        {
+            /* code */
+        }
+        if ((punto_actual.x < grayImage.rows && punto_actual.y < grayImage.cols))
+        {
+            /* code */
+        }
+
+        lista_intensidades[0] = image_segmentated.at<Vec3b>(punto_actual.y - 1, punto_actual.x - 1)[0];
+        lista_intensidades[1] = image_segmentated.at<Vec3b>(punto_actual.y - 1, punto_actual.x)[0];
+        lista_intensidades[2] = image_segmentated.at<Vec3b>(punto_actual.y - 1, punto_actual.x)[0];
+        lista_intensidades[3] = image_segmentated.at<Vec3b>(punto_actual.y - 1, punto_actual.x)[0];
+        lista_intensidades[4] = image_segmentated.at<Vec3b>(punto_actual.y - 1, punto_actual.x)[0];
+        lista_intensidades[5] = image_segmentated.at<Vec3b>(punto_actual.y - 1, punto_actual.x)[0];
+        lista_intensidades[6] = image_segmentated.at<Vec3b>(punto_actual.y - 1, punto_actual.x)[0];
+        lista_intensidades[7] = image_segmentated.at<Vec3b>(punto_actual.y - 1, punto_actual.x)[0];
+    } while (colaPixeles.empty());
+
+    return image_segmentated;
 }
